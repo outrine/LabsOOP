eba96a927090fbfb32a5edfa2b28bda573dbe183
Тимур Городный <61611150+outrine@users.noreply.github.com>
2020-06-03 00:55:05 +0600

    lab4
    Add new labs

--- main.kt
+++ main.kt
@@ -1,94 +1,14 @@
 import data.StudentList
-import data.Students
-import kotlinx.html.js.h1
-import kotlinx.html.js.li
-import kotlinx.html.js.ol
-import kotlinx.html.js.onClickFunction
-import kotlinx.html.dom.append
-import org.w3c.dom.events.Event
+import react.dom.h1
+import react.dom.render
 import kotlin.browser.document
-import kotlin.dom.clear
-import org.w3c.dom.HTMLElement
-import kotlinx.html.js.*
-import kotlinx.html.InputType
-import kotlinx.html.*
 
-var ascending = true
 
 fun main() {
-    document.getElementById("root")!!
-
-        .append {
-            h1 {
-                +"Students"
-                onClickFunction = onCLickFunction()
-            }
-            ol {
-                attributes += "id" to "listStudents"
-                StudentList.map {
-                    li {
-                        attributes += "id" to it.firstname
-                        +"${it.firstname} ${it.surname}"
-                        onClickFunction = onClickFunctionCheck(it)
-                    }
-                }
-            }
-            input(options = arrayListOf("white","tomato","pink"))
+    render(document.getElementById("root")!!) {
+        h1 {
+            +"Students"
         }
-}
-
-
-private fun LI.onClickFunctionCheck(Student :Students): (Event) -> Unit {
-    return{
-        val student = document.getElementById(Student.firstname)!!
-        if (Student.in_place) {
-            student.setAttribute("style", "color:grey")
-            Student.in_place = false
-        }
-        else {
-            student.setAttribute("style", "color:newColor")
-            Student.in_place = true
+            rstudentList(StudentList.toTypedArray())
         }
     }
-}
-
-private fun H1.onCLickFunction (): (Event) -> Unit {
-    return {
-        val listStudents = document.getElementById("listStudents")!!
-        listStudents.clear()
-        listStudents.append {
-            if (ascending)
-                StudentList.sortBy { it.firstname }
-            else
-                StudentList.sortByDescending { it.firstname }
-
-            ascending = !ascending
-
-            StudentList.map {
-                li {
-                    attributes += "id" to it.firstname
-                    +"${it.firstname} ${it.surname}"
-                    onClickFunction = onClickFunctionCheck(it)
-                }
-            }
-        }
-    }
-}
-
-fun TagConsumer<HTMLElement>.input(options : List<String>,
-                                   classes : String? = null,
-                                   block : P.() -> Unit = {})
-        : HTMLElement = p(classes) {
-    options.forEach {
-        +it
-        input (InputType.radio, name = "colors"){
-            value = it
-            onClickFunction = {
-                val color =  document.getElementById("root")!!
-                color.setAttribute("style","color: ${value}")
-            }
-        }
-    }
-    block()
-}
-
